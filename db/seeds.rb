46238a304cf5bd9bf96d39adec4b96b09e729896
Mark <mascottsd@gmail.com>
2016-09-23 13:24:08 -0600

    Five to Thrive replaced & try for seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,42 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-u=User.find_by_email ''
+
+Tenant.create([{
+  tenant_name: 'Water Tracker',
+  subdomain: 'app',
+  desktop: 'desktop.jpg',
+  login: 'login.png',
+  logo: 'logo.png'
+}]);
+
+User.create( [{
+	user_id: 'F01235D0287011E6AEA7123HEF2B3BD8',
+	email: 'aaron@buildit.io',
+	password: 'pass',
+	first_name: 'Aaron',
+	last_name: 'Henderson',
+	gender: 'male',
+	confirmed_at: DateTime.now,
+	is_admin: true,
+	subdomain: 'app'
+},{
+	user_id: 'F02755D384123E6AEA7ATOHEF2B3BD83',
+	email: 'mascottsd@gmail.com',
+	password: "pass",
+	first_name: 'Mark',
+	last_name: 'Scott',
+	gender: 'male',
+	confirmed_at: DateTime.now,
+	is_admin: true,
+	subdomain: 'app'
+}]);
+
+Admin.create([{
+  admin_id: "F99345D0287011E6AEA7A8667F2B3BC9",
+	email: 'mascottsd@gmail.com',
+	password: "pass",
+  first_name: "Mark",
+  last_name: "Scott",
+  confirmed_at: DateTime.now
+}]);
